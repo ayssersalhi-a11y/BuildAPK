d3dcf68cd2e0dcd612a5ac73323d0b5d98e663b3
ayssersalhi-a11y <ayssersalhi@gmail.com>
2026-06-28 14:12:11 +0100

    Update install_template.py

--- scripts/install_template.py
+++ scripts/install_template.py
@@ -42,6 +42,7 @@ else:
 
 print("Using version:", version)
 
+# كتابة .build_version في مكانه الصحيح
 with open(".build_version", "w") as f:
     f.write(version)
 
@@ -50,7 +51,16 @@ with open(".build_version", "r") as f:
 print("build_version:", repr(content))
 print("size:", os.path.getsize(".build_version"), "bytes")
 
-# أخبر Godot أن android template مثبت
+# الإصلاح الجوهري: إنشاء ملف _was_built داخل android/build
+# هذا ما يبحث عنه Godot للتأكد من تثبيت الـ template
+with open("_was_built", "w") as f:
+    f.write("")
+print("_was_built created")
+
+# أعد للمسار الأصلي
+os.chdir(cwd)
+
+# أخبر Godot أن android template مثبت عبر .godot folder
 godot_dir = cwd + "/khayel/.godot"
 os.makedirs(godot_dir, exist_ok=True)
 
@@ -58,5 +68,10 @@ android_cfg = godot_dir + "/android_build_version.cfg"
 with open(android_cfg, "w") as f:
     f.write(version)
 
-print("Godot android config written:", android_cfg)
-print("content:", version)
+print("android_build_version.cfg written:", version)
+
+# تحقق نهائي
+print("=== التحقق النهائي ===")
+print("build_version:", open(build_dir + "/.build_version").read())
+print("_was_built:", os.path.exists(build_dir + "/_was_built"))
+print("android_build_version.cfg:", open(android_cfg).read())
